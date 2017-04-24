293a476d509b2f60a010c2ae5192596c26c904e0
Nitzan Jaitman <nitzan@cloudinary.com>
2017-04-24 10:20:55 +0300

    Configure travis to ignore test methods with 'Timeout' in their name.

--- cloudinary-http42/src/test/java/com/cloudinary/test/ApiTest.java
+++ cloudinary-http42/src/test/java/com/cloudinary/test/ApiTest.java
@@ -9,13 +9,13 @@ import org.junit.Test;
 import org.apache.http.conn.ConnectTimeoutException;
 
 public class ApiTest extends AbstractApiTest {
-//    @Test(expected = ConnectTimeoutException.class)
-//    public void testTimeoutException() throws Exception {
-//        // should allow listing resources
-//        Map<String, Object> options = new HashMap<String, Object>();
-//        options.put("timeout", Integer.valueOf(100));
-//
-//        Map result = api.resources(options);
-//        Map resource = findByAttr((List<Map>) result.get("resources"), "public_id", "api_test");
-//    }
+    @Test(expected = ConnectTimeoutException.class)
+    public void testTimeoutException() throws Exception {
+        // should allow listing resources
+        Map<String, Object> options = new HashMap<String, Object>();
+        options.put("timeout", Integer.valueOf(1));
+
+        Map result = api.resources(options);
+        Map resource = findByAttr((List<Map>) result.get("resources"), "public_id", "api_test");
+    }
 }
